523975aa4f673221701a378ece0da967f1261ef4
Qinlan Shen <qinlans@misty.lti.cs.cmu.edu>
2018-03-12 11:48:31 -0400

    Add baseline

---
+++ attention.py
@@ -0,0 +1,230 @@
+import csv
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from collections import defaultdict
+from nltk.tokenize import TweetTokenizer
+from torch.autograd import Variable
+from torch import optim
+
+use_cuda = torch.cuda.is_available()
+HIDDEN_DIM = 32 
+MAX_LENGTH = 140
+labels = {'none': 0, 'racism': 1, 'sexism': 2}
+
+# Class for converting from words to ids and vice-versa
+class Vocab:
+    def __init__(self):
+        self.word_to_id = defaultdict(lambda: 0)
+        self.id_to_word = []
+        self.word_to_id['<UNK>'] = 0
+        self.id_to_word.append('<UNK>')
+        self.word_to_id['<SENT>'] = 1
+        self.id_to_word.append('<SENT>')
+        self.word_to_id['</SENT>'] = 2
+        self.id_to_word.append('</SENT>')
+
+    # Given a corpus, build token to id and id to token dictionaries for words
+    # that occur more frequently than unk_threshold
+    def build_vocab(self, corpus, unk_threshold=1):
+        word_count = defaultdict(lambda: 0)
+        for text in corpus:
+            for word in text:
+                word_count[word] += 1
+
+        for word, count in word_count.items():
+            if count > unk_threshold and not word in self.word_to_id:
+                self.word_to_id[word] = len(self.word_to_id)
+                self.id_to_word.append(word)
+
+    def get_word_id(self, word):
+        return self.word_to_id[word]
+
+    def get_word_from_id(self, word_id):
+        return self.id_to_word[word_id]
+
+    def size(self):
+        return len(self.id_to_word)
+
+class BidirectionalEncoder(nn.Module):
+    def __init__(self, vocab_size, hidden_size):
+        super(BidirectionalEncoder, self).__init__()
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(self.vocab_size, self.hidden_size)
+        self.bi_lstm = nn.LSTM(self.hidden_size, self.hidden_size,
+            bidirectional=True)
+
+    def forward(self, instance_input, hidden):
+        embedded = self.embedding(instance_input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.bi_lstm(output, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        if use_cuda:
+            result = (Variable(torch.zeros(2, 1, self.hidden_size)).cuda(), 
+                Variable(torch.zeros(2, 1, self.hidden_size)).cuda())
+        else:
+            result = (Variable(torch.zeros(2, 1, self.hidden_size)),
+                Variable(torch.zeros(2, 1, self.hidden_size)))
+
+        return result
+
+class AttentionDecoder(nn.Module):
+    def __init__(self, num_labels, hidden_size, max_length):
+        super(AttentionDecoder, self).__init__()
+        self.num_labels = num_labels
+        self.hidden_size = hidden_size
+        self.max_length = max_length
+
+        self.attn = nn.Linear(self.hidden_size*2, self.max_length)
+        self.layer1 = nn.Linear(self.hidden_size*2, self.hidden_size)
+        self.layer2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.num_labels)
+
+    def forward(self, encoder_outputs, encoder_hidden):
+        attn_weights = F.softmax(self.attn(encoder_hidden[0].view(-1, self.hidden_size * 2)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+            encoder_outputs.unsqueeze(0))
+        output = F.relu(self.layer1(attn_applied))
+        output = F.tanh(self.layer2(output))
+        output = F.softmax(self.out(output[0]), dim=1)
+
+        return output, attn_weights
+
+# Preprocesses text for model input
+def process_raw_text(raw_text):
+    tokenizer = TweetTokenizer()
+    text = tokenizer.tokenize(raw_text.lower())
+    text = ['<SENT>'] + text
+    text.append('</SENT>')
+    return text
+
+# Read in file containing text for building the training vocabulary
+def read_corpus_file(corpus_filename):
+    corpus = []
+    tokenizer = TweetTokenizer()
+    reader = csv.DictReader(open(corpus_filename, 'r'))
+    for row in reader:
+        text = process_raw_text(row['text'])
+        corpus.append(text)
+
+    return corpus
+
+# Read file containing text and label pairs and converts them into Variables
+def process_instances(instances_filename, vocab, labels):
+    instances = []
+    reader = csv.DictReader(open(instances_filename, 'r'))
+    for row in reader:
+        text = process_raw_text(row['text'])
+        text_ids = [vocab.get_word_id(word) for word in text]
+        text_variable = Variable(torch.LongTensor(text_ids).view(-1, 1))
+        label = Variable(torch.LongTensor([labels[row['label']]]))
+        if use_cuda:
+            text_variable = text_variable.cuda()
+            label = label.cuda()
+
+        instances.append((text_variable, label))
+
+    return instances
+
+# Update the model for the given instance
+def update_model(instance, encoder, encoder_optimizer, decoder, decoder_optimizer,
+    criterion, max_length):
+    encoder_hidden = encoder.init_hidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    instance_input, instance_label = instance
+    input_length = instance_input.size()[0]
+
+    encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size*2))
+    encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            instance_input[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0][0]
+
+    decoder_output, decoder_attention = decoder(encoder_outputs, encoder_hidden)
+    loss = criterion(decoder_output, instance_label)
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss
+
+# Trains the model over training_instances for a given number of epochs
+def train_epochs(training_instances, encoder, decoder, vocab, labels, max_length, n_epochs=10,
+    print_every=500, learning_rate=0.1):
+    
+    print_loss_total = 0
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    criterion = torch.nn.CrossEntropyLoss()
+    for i in range(n_epochs):
+        print_epoch_loss_total = 0
+        print 'Training epoch ' + str(i)
+        random.shuffle(training_instances)
+        for j, instance in enumerate(training_instances):
+            loss = update_model(instance, encoder, encoder_optimizer, decoder, decoder_optimizer,
+                criterion, max_length)
+            print_loss_total += loss
+            print_epoch_loss_total += loss
+
+            if j % print_every == 0:
+                print_loss_avg = print_loss_total/print_every
+                print_loss_total = 0
+                print('Epoch %d iteration %d loss: %.4f' % (i, j, print_loss_avg))
+
+        print_epoch_loss_avg = print_epoch_loss_total/len(training_instances)
+        decode(training_instances, encoder, decoder, vocab, labels, max_length)
+        print('Epoch %d avg loss: %.4f' % (i, print_epoch_loss_avg))
+
+# Evaluates the model on the given instance_inputs
+def decode(instances, encoder, decoder, vocab, labels, max_length):
+    for instance_input, _ in instances:
+        encoder_hidden = encoder.init_hidden()
+        input_length = instance_input.size()[0]
+
+        encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size*2))
+        encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(
+                instance_input[ei], encoder_hidden)
+            encoder_outputs[ei] = encoder_output[0][0]
+
+        decoder_output, decoder_attention = decoder(encoder_outputs, encoder_hidden)
+        top_value, top_label = decoder_output.data.topk(1)
+
+def main():
+    training_filename = 'zeerak_naacl/test.csv'
+    dev_filename = 'zeerak_naacl/zeerak_naacl_tweets.csv'
+    test_filename = 'zeerak_naacl/zeerak_naacl_tweets.csv' 
+
+    training_corpus = read_corpus_file(training_filename)
+    vocab = Vocab()
+    vocab.build_vocab(training_corpus)
+
+    training_instances = process_instances(training_filename, vocab, labels)
+    """
+    dev_instances = process_instances(dev_filename, vocab, labels)
+    test_instances = process_instances(test_filename, vocab, labels)
+    """
+
+    encoder = BidirectionalEncoder(vocab.size(), HIDDEN_DIM)
+    decoder = AttentionDecoder(len(labels), HIDDEN_DIM, MAX_LENGTH)
+    if use_cuda:
+        encoder = encoder.cuda()
+        decoder = decoder.cuda()
+
+    train_epochs(training_instances, encoder, decoder, vocab, labels, MAX_LENGTH)
+
+if __name__ == '__main__': main()
